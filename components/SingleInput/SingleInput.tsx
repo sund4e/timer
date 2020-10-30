434bcfc68c37aad631b8d6e3cfd61002f76f6aac
sund4e <suvi@smartly.io>
2020-10-30 15:57:35 +0200

    Use div instead of input
    handling the cursor is pain in the a**

--- components/SingleInput/SingleInput.tsx
+++ components/SingleInput/SingleInput.tsx
@@ -2,15 +2,22 @@ import styled from 'styled-components';
 import { useRef, useEffect, useState } from 'react';
 import { Theme } from '../../styles/theme';
 
-const StyledInput = styled.input`
+const StyledInput = styled.div`
   color: ${({ theme, isFocused }: { theme: Theme; isFocused: boolean }) =>
     isFocused ? theme.colors.accent : theme.colors.primary};
   border-style: none;
   font-size: ${({ theme }: { theme: Theme }) => theme.fontSizes.big};
-  caret-color: transparent;
+  caret-color: transparents;
   background-color: transparent;
   &:focus {
     outline: none;
+  }
+  flex-grow: 0;
+  ::-moz-selection {
+    color: red;
+  }
+  ::selection {
+    color: red;
   }
 `;
 
@@ -34,30 +41,37 @@ const SingleInput = ({ value, onChange, isFocused, onClick }: Props) => {
     }
   }, [isFocused]);
 
-  useEffect(() => {
-    input.current && input.current.setSelectionRange(0, 0);
-  });
-
-  const inputValue = value.toString();
-
   const onChangeInput = (event: React.FormEvent<HTMLInputElement>) => {
     const newValue = event.currentTarget.value;
-    const newTime = parseInt(newValue[0]);
+    const newTime = parseInt(newValue[1]);
 
     if (!isNaN(newTime)) {
       onChange(newTime);
     }
   };
 
+  const onClickInput = () => {
+    onClick();
+  };
+
+  const onKeyPress = (event: React.KeyboardEvent) => {
+    const number = parseInt(event.key);
+    if (!isNaN(number)) {
+      onChange(number);
+    }
+  };
+
   return (
     <StyledInput
       ref={input}
-      value={inputValue}
       onChange={onChangeInput}
-      size={1}
       isFocused={isFocused}
-      onClick={onClick}
-    />
+      onClick={onClickInput}
+      tabIndex={0}
+      onKeyPress={onKeyPress}
+    >
+      {value}
+    </StyledInput>
   );
 };
 
