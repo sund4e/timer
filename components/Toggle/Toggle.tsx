da36ea52e65067d4c467c47ff279109d162f5eda
sund4e <suvi@smartly.io>
2020-11-20 10:50:01 +0200

    Fine tune toggle

--- components/Toggle/Toggle.tsx
+++ components/Toggle/Toggle.tsx
@@ -1,20 +1,35 @@
 import styled from 'styled-components';
+import { Theme } from '../../styles/theme';
 
-const Label = styled.label`
+const getToggleWidth = (theme: Theme) => theme.fontSizes.medium * 1.7;
+
+const getToggleHeight = (theme: Theme) => theme.fontSizes.medium;
+
+const getToggleButtonDiameter = (theme: Theme) => theme.fontSizes.medium * 0.7;
+
+const getToggleButtonMargin = (theme: Theme) =>
+  (getToggleHeight(theme) - getToggleButtonDiameter(theme)) / 2;
+
+const getToggleButtonTranslate = (theme: Theme) =>
+  getToggleWidth(theme) -
+  getToggleButtonMargin(theme) * 2 -
+  getToggleButtonDiameter(theme);
+
+const ToggleButton = styled.label`
   position: relative;
   display: inline-block;
 `;
 
 const Wrapper = styled.div`
-  height: ${({ theme }) => theme.fontSizes.medium}em;
-  padding: ${({ theme }) => theme.fontSizes.medium * 0.25}em;
+  height: ${({ theme }) => getToggleHeight(theme)}rem;
+  padding: ${({ theme }) => getToggleHeight(theme) * 0.25}rem;
 `;
 
 const Span = styled.span`
   position: absolute;
   cursor: pointer;
-  width: ${({ theme }) => theme.fontSizes.medium * 1.7}em;
-  height: ${({ theme }) => theme.fontSizes.medium}em;
+  width: ${({ theme }) => getToggleWidth(theme)}rem;
+  height: ${({ theme }) => getToggleHeight(theme)}rem;
   top: 0;
   left: 0;
   right: 0;
@@ -28,13 +43,13 @@ const Span = styled.span`
     border-radius: 50%;
     position: absolute;
     content: '';
-    height: ${({ theme }) => theme.fontSizes.medium * 0.7}em;
-    width: ${({ theme }) => theme.fontSizes.medium * 0.7}em;
-    left: ${({ theme }) => theme.fontSizes.medium * 0.15}em;
-    bottom: ${({ theme }) => theme.fontSizes.medium * 0.15}em;
+    height: ${({ theme }) => getToggleButtonDiameter(theme)}rem;
+    width: ${({ theme }) => getToggleButtonDiameter(theme)}rem;
+    left: ${({ theme }) => getToggleButtonMargin(theme)}rem;
+    bottom: ${({ theme }) => getToggleButtonMargin(theme)}rem;
     background-color: ${({ theme }) => theme.colors.light};
-    -webkit-transition: 0.4s;
-    transition: 0.4s;
+    -webkit-transition: ${({ theme }) => theme.transition}s;
+    transition: ${({ theme }) => theme.transition}s;
   }
 `;
 
@@ -47,10 +62,12 @@ const Input = styled.input`
   }
   :checked + ${Span}:before {
     -webkit-transform: translateX(
-      ${({ theme }) => theme.fontSizes.medium * 0.7}em
+      ${({ theme }) => getToggleButtonTranslate(theme)}rem
     );
-    -ms-transform: translateX(${({ theme }) => theme.fontSizes.medium * 0.7}em);
-    transform: translateX(${({ theme }) => theme.fontSizes.medium * 0.7}em);
+    -ms-transform: translateX(
+      ${({ theme }) => getToggleButtonTranslate(theme)}rem
+    );
+    transform: translateX(${({ theme }) => getToggleButtonTranslate(theme)}rem);
   }
   :focus + ${Span} {
     box-shadow: 0 0 1px ${({ theme }) => theme.colors.highlight};
@@ -58,9 +75,9 @@ const Input = styled.input`
 `;
 
 const Text = styled.span`
-  position: relative;
-  left: ${({ theme }) => theme.fontSizes.medium * 1.8}em;
-  top: 8px;
+  padding-left: ${({ theme }) => getToggleWidth(theme)}rem;
+  line-height: ${({ theme }) => getToggleHeight(theme)}rem;
+  vertical-align: middle;
 `;
 
 export type Props = {
@@ -75,10 +92,10 @@ const Toggle = ({ isOn, setIsOn, label }: Props) => {
   };
   return (
     <Wrapper>
-      <Label>
+      <ToggleButton>
         <Input type="checkbox" checked={isOn} onChange={onChange} />
         <Span />
-      </Label>
+      </ToggleButton>
       <Text>{label}</Text>
     </Wrapper>
   );
