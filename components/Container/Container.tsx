0d99d4900541f4e69e8c2e90e70a0e2c1b3f57ed
sund4e <suvi@smartly.io>
2020-12-04 09:48:52 +0200

    Change background color into less noticable

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,11 +1,12 @@
 import styled from 'styled-components';
+import { useState, useEffect } from 'react';
 
 const Container = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   height: 100vh;
-  background-color: ${({ theme }) => theme.colors.light};
+  background-color: #a2a8b3;
   background-image: url('./luirojarvi.jpg');
   background-size: cover;
   color: ${({ theme }) => theme.colors.primary};
