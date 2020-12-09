9fc02bdecffa458963ba0378cdc611c49207184f
sund4e <suvi@smartly.io>
2020-12-09 18:47:52 +0200

    Use low resolution image for initial render

--- components/Container/Container.tsx
+++ components/Container/Container.tsx
@@ -1,15 +1,23 @@
 import styled from 'styled-components';
 import { useState, useEffect } from 'react';
 
-const Container = styled.div`
+const StyledContainer = styled.div`
   display: flex;
   justify-content: center;
   flex-direction: column;
   height: 100vh;
   background-color: #a2a8b3;
-  background-image: url('./luirojarvi.jpg');
+  background-image: url(${({ image }: { image: any }) => image});
   background-size: cover;
   color: ${({ theme }) => theme.colors.primary};
 `;
+
+const Container = ({ children }: { children: React.ReactNode }) => {
+  const [imageUrl, setImageUrl] = useState('./luirojarvi_small.jpg');
+  useEffect(() => {
+    setImageUrl('./luirojarvi.jpg');
+  }, []);
+  return <StyledContainer image={imageUrl}>{children}</StyledContainer>;
+};
 
 export default Container;
