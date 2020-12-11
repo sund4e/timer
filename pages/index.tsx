de5badd442c0f0d9532e2e99657574e990b23bba
sund4e <suvi@smartly.io>
2020-12-11 14:16:25 +0200

    Add umami tracking

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,6 +15,12 @@ const Home = () => {
           to set recurring reminders. Free and no ads."
         />
         <link rel="icon" href="/favicon.ico" />
+        <script
+          async
+          defer
+          data-website-id="f3044ce1-5436-4a7d-9477-15b05eb0e6ef"
+          src="https://umami-kappa.vercel.app/umami.js"
+        ></script>
       </Head>
       <Container>
         {/* <Title>{time}</Title> */}
