dd36b97951c341fbc53cdfd14380392b070e8f07
Suvi Valamäki <suvmakinen@gmail.com>
2025-05-27 11:43:51 +0300

    Use es modules
    fixes e.g. linter in editor

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,4 @@
-module.exports = {
+export default {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['<rootDir>/.next/', '<rootDir>/node_modules/'],
   setupFilesAfterEnv: ['<rootDir>/setupTests.js'],
