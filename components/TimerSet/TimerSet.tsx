eddac665a6dd29070110afdcf18fe21f73d45225
Suvi Valamäki <suvmakinen@gmail.com>
2025-05-27 15:30:56 +0300

    Fix controls position

--- components/TimerSet/TimerSet.tsx
+++ components/TimerSet/TimerSet.tsx
@@ -87,7 +87,7 @@ const TimerSetControls = styled.div<{
   justify-content: center;
   transition: transform ${({ theme }) => theme.transition}s ease-out;
   ${({ $timersLength, $currentTimerIndex }) =>
-    `transform: translateY(${Math.min(visibleTimers, $timersLength - $currentTimerIndex) * fontSizeInactive + fontSizeActive / 2 + 1}vh);`}
+    `transform: translateY(${Math.min(visibleTimers + 1, $timersLength - $currentTimerIndex) * fontSizeInactive + fontSizeActive / 2}vh);`}
   gap: 10px;
   margin-top: 1vh;
 `;
